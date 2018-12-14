994ea21d36c9a8d377d67c2b6d2917b5af62c956
Alex <alexandru.dascalescu90@gmail.com>
2018-12-14 10:57:22 +0200

    firebase database modified

--- src/firebase/db.jsx
+++ src/firebase/db.jsx
@@ -8,6 +8,22 @@ export const doCreateUser = (id, firstName, lastName, email) => {
     });
 }
 
+
 export const onceGetUsers = () => {
     db.ref('users').once('value');
 }
+
+
+export const doCreateCompany = (companyName,companyDescription) => {
+    db.ref(`companies`).push({
+        companyName,
+        companyDescription
+    });
+}
+
+export const doCreateService = (serviceDescription,serviceName,spaces,duration,price,availability) => {
+    db.ref(`services`).push({
+        serviceDescription,serviceName,duration,spaces,price,availability
+    });
+}
+
