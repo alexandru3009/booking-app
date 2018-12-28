9fb22e0efd7cee2e3aedcba59f5f8afefa71fb75
Alex <alexandru.dascalescu90@gmail.com>
2018-12-28 14:15:15 +0200

    small style Nav comp

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import book from './book.svg';
+import './Navigation.css'
 import { NavLink } from 'react-router-dom';
 import SignOutButton from '../auth/Logout';
 import AuthUserContext from '../Authentication/AuthUserContext';
 
-const Navigation = ({ authUser }) => (
+const Navigation = () => (
     <header>
-        <div>
-            <h1>Booking application</h1>    
+        <div className="header-style">
+        <h1>Booking application</h1>
+        <img src={book} alt="book" />
             <AuthUserContext.Consumer>
                 { authUser => authUser ? <NavigationAuth/> : <NavigationNonAuth/> }
             </AuthUserContext.Consumer>
@@ -27,7 +30,7 @@ const NavigationAuth = () => (
 );
 const NavigationNonAuth = () => (
     <header>
-        <NavLink to="/" activeClassName="is-active">Home</NavLink>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
         <NavLink to="/register" activeClassName="is-active">Register account</NavLink>
         <NavLink to="/login" activeClassName="is-active">Login</NavLink>
         <NavLink to="/recover" activeClassName="is-active">  Recover Password  </NavLink>
